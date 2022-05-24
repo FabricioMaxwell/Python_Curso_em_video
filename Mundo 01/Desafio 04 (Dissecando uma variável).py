2d89681edbe7289cc0a4a76f87f8994e2cba5fcf
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2022-05-23 23:26:35 -0300

    Dafios de 02 a 04

---
+++ Mundo 01/Desafio 04 (Dissecando uma variável).py
@@ -0,0 +1,8 @@
+n = input('digite algo: ')
+print(f'O tipo primitivo desse valor é {type(n)}')
+print(f'Só tem espaços? {n.isspace()}')
+print(f'É um número? {n.isnumeric()}')
+print(f'É alfabeto? {n.isalpha()}')
+print(f'Está em letra maiúscula? {n.isupper()}')
+print(f'Está em letra minúscula? {n.islower()}')
+print(f'Está capitalizado? {n.istitle()}')
