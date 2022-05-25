a74eab9f3087a64a05d54ef3b50a1cb803310940
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2022-05-25 00:24:12 -0300

    Desafio 19 a 21

--- Mundo 01/Desafio 20 (sorteando na ordem da lista).py
+++ Mundo 01/Desafio 20 (sorteando na ordem da lista).py
@@ -1,8 +1,9 @@
-from random import choice
+import random
 aluno1 = str(input('Primeiro aluno: '))
 aluno2 = str(input('Segundo aluno: '))
 aluno3 = str(input('Terceiro aluno: '))
 aluno4 = str(input('Quarto aluno: '))
 lista = [aluno1, aluno2, aluno3, aluno4]
-escolhido = choice(lista)
-print(f'O aluno escolhido foi {escolhido}')
+random.shuffle(lista)
+print('A ordem de apresentação será')
+print(lista)
