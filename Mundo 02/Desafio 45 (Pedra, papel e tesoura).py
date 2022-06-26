c00f61ae3a975180e097b10038d57edfa0566cd5
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2022-06-25 23:51:28 -0300

    Desafio 45 atualização

--- Mundo 02/Desafio 45 (Pedra, papel e tesoura).py
+++ Mundo 02/Desafio 45 (Pedra, papel e tesoura).py
@@ -1,10 +1,13 @@
 from random import randint
 from time import sleep
+
+print('-=' * 12, 'BEM VINDO AO JO KEN PO!', '-=' * 12)
+
 print("""Sua opções
 [ 0 ] PEDRA
 [ 1 ] PAPEL
 [ 2 ] TESOURA""")
-jogada = int(input('Qual é a sua jogada? '))
+jogador = int(input('Qual é a sua jogada? '))
 opçoes = ['pedra', 'papel', 'tesoura']
 computador = randint(0, 2)
 
@@ -14,13 +17,52 @@ print('KEN')
 sleep(1)
 print('PO!!')
 
-print('-=' * 12)
-print(f'Computador jogou {opçoes[computador]}')
-print(f'Jogador jogou {opçoes[jogada]}')
-print('-=' * 12)
 
-if computador == 0:
-    if jogador 
+if computador == jogador: # Jogaram iguais
+    print('EMPATE')
 
-print()
+elif computador == 0: #Computador jogou Pedra
+    if jogador == 1:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12)
+        print('jOGADOR VENCE!')
+    else:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12) 
+        print('COMPUTADOR VENCE!')
+
+elif computador == 1: #Computador jogou Papel
+    if jogador == 0:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12)
+        print('COMPUTADOR VENCE!')
+    else:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12)
+        print('JOGADOR VENCE!')
+
+elif computador == 2: #Computador jogou Tesoura
+    if jogador == 1:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12)
+        print('COMPUTADOR VENCE!')
+    else:
+        print('-=' * 12)
+        print(f'Computador jogou {opçoes[computador]}')
+        print(f'Jogador jogou {opçoes[jogador]}')
+        print('-=' * 12)
+        print('JOGADOR VENCE')
+
+else:
+    print('JOGADA INVÁLIDA!')
 
