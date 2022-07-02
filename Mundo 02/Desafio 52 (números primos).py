ec0e258a70905107dbf5e975a679ee984baf7d1f
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2022-07-01 23:03:27 -0300

    Desafio 52

--- Mundo 02/Desafio 52 (números primos).py
+++ Mundo 02/Desafio 52 (números primos).py
@@ -7,8 +7,8 @@ for c in range(1, n + 1):
     else:
         print(f'\033[1;91m{c}\033[m', end=' ')
 print(f'\nO número {n} foi divisível {divisivel} vezes')
+
 if divisivel == 2:
      print('E por isso ele É PRIMO!')
 else:
     print('E por isso ele NÃO É PRIMO!')   
-    
